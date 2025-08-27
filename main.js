538205e7d39fb0c3290679b73d542e1a89ebaf8f
Prateek Gupta <114770325+guptaprateek112@users.noreply.github.com>
2025-08-27 23:26:21 +0530

    main.js

--- main.js
+++ main.js
@@ -76,3 +76,16 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-theme', getCurrentTheme())
   localStorage.setItem('selected-icon', getCurrentIcon())
 })
+// Mobile menu toggle
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId);
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show');
+        });
+    }
+}
+showMenu('nav-toggle','nav-menu');
+
